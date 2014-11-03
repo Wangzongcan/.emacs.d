05a137e421255885d04a0a564a87b6a9daabbc8c
Wangzongcan <wang.zongcan@gmail.com>
2014-11-03 15:13:06 +0800

    update

--- inits/11_parens.el
+++ inits/11_parens.el
@@ -1,6 +1,6 @@
+(require 'smartparens-config)
 (smartparens-global-mode t)
 (show-smartparens-global-mode t)
 
-(setq sp-highlight-pair-overlay nil)
 (setq sp-ignore-modes-list '(minibuffer-inactive-mode
                              web-mode))
