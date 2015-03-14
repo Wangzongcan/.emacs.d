ba6af5f2af383d5ca0bc216287e892817d2db893
Wangzongcan <wang.zongcan@gmail.com>
2015-03-15 00:06:53 +0800

    format

--- config/init-multi-term.el
+++ config/init-multi-term.el
@@ -2,7 +2,7 @@
 
 (require 'multi-term)
 
-(setq multi-term-program "/bin/bash")
-(setq multi-term-dedicated-select-after-open-p t)
+(setq multi-term-program "/bin/bash"
+      multi-term-dedicated-select-after-open-p t)
 
 (provide 'init-multi-term)
