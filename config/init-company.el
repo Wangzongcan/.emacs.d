4564561628835baf1b72fcdf63231c420be00c7d
Wangzongcan <wang.zongcan@gmail.com>
2015-03-29 22:29:46 +0800

    update company-mode

--- config/init-company.el
+++ config/init-company.el
@@ -2,20 +2,16 @@
 
 (require 'company)
 
-(defun complete-or-indent ()
-  (interactive)
-  (if (company-manual-begin)
-      (company-complete-common)
-    (indent-according-to-mode)))
-
 (setq company-idle-delay nil
-      company-auto-complete nil)
+      company-auto-complete nil
+      company-dabbrev-downcase nil
 
-(define-key global-map "\t" 'complete-or-indent)
+      company-tooltip-limit 10)
+
+(define-key global-map (kbd "<backtab>") 'company-complete)
 (define-key company-active-map (kbd "\C-n") 'company-select-next)
 (define-key company-active-map (kbd "\C-p") 'company-select-previous)
 (define-key company-active-map (kbd "\C-g") 'company-abort)
-
 
 (add-hook 'after-init-hook 'global-company-mode)
 
