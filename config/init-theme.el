4a65b5643d8994f63a17967ea6d69dba4d4826ea
Wangzongcan <wang.zongcan@gmail.com>
2015-04-10 10:10:42 +0800

    change theme

--- config/init-theme.el
+++ config/init-theme.el
@@ -1,14 +1,6 @@
 ;; Theme
-;; (package-install 'monokai-theme)
+(package-install 'monokai-theme)
 
-;; (load-theme 'monokai t)
-
-(package-install 'solarized-theme)
-
-;; make the fringe stand out from the background
-(setq solarized-distinct-fringe-background t
-      x-underline-at-descent-line t)
-
-(load-theme 'solarized-light t)
+(load-theme 'monokai t)
 
 (provide 'init-theme)
