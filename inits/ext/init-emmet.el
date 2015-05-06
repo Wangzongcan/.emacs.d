c7f3bb8d9a45d45bd3692e24cd85f8f477d492ae
Wangzongcan <wang.zongcan@gmail.com>
2015-05-06 14:36:24 +0800

    emmet mode 取消 C-return 的映射

--- inits/ext/init-emmet.el
+++ inits/ext/init-emmet.el
@@ -1,8 +1,11 @@
+(require 'emmet-mode)
+
 (add-hook 'web-mode-hook 'emmet-mode)
 (add-hook 'css-mode-hook 'emmet-mode)
 (add-hook 'scss-mode-hook 'emmet-mode)
 (add-hook 'sass-mode-hook 'emmet-mode)
 (add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2)))
 
-(require 'el-init)
-(el-init-provide)
+(define-key emmet-mode-keymap (kbd "<C-return>") 'nil)
+
+(provide 'init-emmet)
