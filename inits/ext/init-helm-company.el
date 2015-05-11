fb76c9d5874159148bd01d223e13be60f1c38935
Wangzongcan <wang.zongcan@gmail.com>
2015-05-11 10:36:22 +0800

    remove helm-company

--- inits/ext/init-helm-company.el
+++
@@ -1,7 +0,0 @@
-(autoload 'helm-company "helm-company")
-(eval-after-load 'company
-  '(progn
-     (define-key company-mode-map (kbd "C-;") 'helm-company)
-     (define-key company-active-map (kbd "C-;") 'helm-company)))
-
-(provide 'init-helm-company)
