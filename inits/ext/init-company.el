91b1e4d57037a24dee03a58b12702e7b58d6be26
Wangzongcan <wang.zongcan@gmail.com>
2015-05-11 10:43:56 +0800

    update company

--- inits/ext/init-company.el
+++ inits/ext/init-company.el
@@ -5,4 +5,9 @@
 
 (add-hook 'after-init-hook 'global-company-mode)
 
+(define-key company-mode-map (kbd "C-;") 'company-complete)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-g") 'company-abort)
+
 (provide 'init-company)
