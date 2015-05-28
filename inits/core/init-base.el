438b2456000d992a6fd2abba9f54c3def01ce845
Wangzongcan <wang.zongcan@gmail.com>
2015-05-28 18:20:15 +0800

    update

--- inits/core/init-base.el
+++ inits/core/init-base.el
@@ -1,72 +1,83 @@
 ;; Encoding
+(prefer-coding-system 'utf-8)
+(setenv "LANG" "en_US.UTF-8")
+(set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (set-language-environment "UTF-8")
-(prefer-coding-system 'utf-8)
-(setenv "LANG" "en_US.UTF-8")
+
+(if (boundp 'buffer-file-coding-system)
+    (setq-default buffer-file-coding-system 'utf-8)
+  (setq default-buffer-file-coding-system 'utf-8))
+
+(setq locale-coding-system 'utf-8)
+
+;; Global Modes
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(blink-cursor-mode -1)
+
+(transient-mark-mode t)
+(global-font-lock-mode t)
+(global-whitespace-mode t)
+(global-auto-revert-mode t)
+
+(electric-indent-mode)
 
 ;; General Settings
 (setq
  inhibit-startup-message t
- initial-scratch-message ""
- visible-bell t
+ initial-scratch-message nil
+
+ visible-bell nil
  ring-bell-function #'ignore
- backup-directory-alist `((".*" . ,temporary-file-directory)) ;don't clutter my fs and put backups into tmp
- auto-save-file-name-transforms `((".*" ,temporary-file-directory t))
- require-final-newline t                ;auto add newline at the end of file
- column-number-mode t                   ;show the column number
- default-major-mode 'text-mode          ;use text mode per default
- mouse-yank-at-point t                  ;middle click with the mouse yanks at point
- history-length 250                     ;default is 30
- locale-coding-system 'utf-8            ;utf-8 is default
- tab-always-indent 'complete            ;try to complete before identing
- confirm-nonexistent-file-or-buffer nil ;don't ask to create a buffer
- vc-follow-symlinks t                   ;follow symlinks automatically
- recentf-max-saved-items 5000           ;same up to 5000 recent files
- eval-expression-print-length nil       ;do not truncate printed expressions
- eval-expression-print-level nil        ;print nested expressions
+
+ default-major-mode 'text-mode         ;; use text mode per default
+
+ global-auto-revert-non-file-buffers t
+ auto-revert-verbose nil
+
+ backup-by-copying t                   ;; backup
+ make-backup-files nil
+ auto-save-default nil
+ create-lockfiles  nil
+
  kill-ring-max 5000                     ;truncate kill ring after 5000 entries
  mark-ring-max 5000                     ;truncate mark ring after 5000 entries
- mouse-wheel-scroll-amount '(1 ((shift) . 5) ((control))) ;make mouse scrolling smooth
- indicate-buffer-boundaries 'left       ;fringe markers
- split-height-threshold 110             ;more readily split horziontally
- enable-recursive-minibuffers t
- custom-unlispify-menu-entries nil      ;M-x customize should not cripple menu entries
- custom-unlispify-tag-names nil         ;M-x customize should not cripple tags
- blink-matching-paren nil
+ recentf-max-saved-items 5000           ;same up to 5000 recent files
+
+ font-lock-maximum-decoration t
+
+ tab-always-indent t
+
  default-truncate-lines t
- echo-keystrokes 0.1
- whitespace-style '(face tabs empty trailing indendation space-after-tab space-before-tab)
+ whitespace-style'
+ '(face tabs empty trailing indendation space-after-tab space-before-tab)
+
  scroll-step 1
  scroll-conservatively 10000
- auto-window-vscroll nil
- font-lock-maximum-decoration t
+
+ require-final-newline t               ;; auto add newline at the end of file
+
+ line-number-mode t                    ;; show line number
+ column-number-mode t                  ;; show column number
+
+ echo-keystrokes 0.1                   ;; minibuffer
+ enable-recursive-minibuffers t
+ minibuffer-prompt-properties
+ '(read-only t point-entered minibuffer-avoid-prompt face minibuffer-prompt)
+
+ confirm-nonexistent-file-or-buffer nil ;don't ask to create a buffer
+
  ad-redefinition-action 'accept
  )
 
-;; Global Modes
-(global-auto-revert-mode 1)  ;auto revert buffers when changed on disk
-(blink-cursor-mode -1)       ;no cursor blinking
-(tool-bar-mode -1)           ;disable the awful toolbar
-(menu-bar-mode -1)           ;no menu
-(scroll-bar-mode -1)         ;disable the sroll bar
-(global-font-lock-mode t)
-(transient-mark-mode t)
-(global-whitespace-mode 1)
-
 ;; Default Settings
-(setq-default
- tab-width 4
- indent-tabs-mode nil                   ;use spaces instead of tabs
- c-basic-offset 4                       ;"tab" with in c-related modes
- c-hungry-delete-key t                  ;delete more than one space
- indicate-empty-lines t
- )
+(setq-default indent-tabs-mode nil                   ;use spaces instead of tabs
+              default-tab-width 2)
 
 ;; Yes or No
 (fset 'yes-or-no-p 'y-or-n-p)
-
-;; startup
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
 
 (provide 'init-base)
