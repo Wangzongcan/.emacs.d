1f124a3f8c47a9f196b40077d417d08670e46bfa
Wangzongcan <wang.zongcan@gmail.com>
2015-06-29 22:16:41 +0800

    update

--- init.el
+++ init.el
@@ -3,22 +3,25 @@
 (require 'init-cask)
 (require 'init-encoding)
 (require 'init-base)
-(require 'init-font)
 (require 'init-ibuffer)
 (require 'init-dired)
-(require 'init-theme)
-(require 'init-smart-mode-line)
+
 (require 'init-evil)
+(require 'init-emmet)
+(require 'init-multiple-cursors)
+
 (require 'init-helm)
 (require 'init-magit)
 (require 'init-projectile)
-(require 'init-multiple-cursors)
-(require 'init-emmet)
 
 (require 'init-ruby)
 (require 'init-web)
 (require 'init-javascript)
 (require 'init-css)
 (require 'init-yaml)
+
+(require 'init-font)
+(require 'init-theme)
+(require 'init-smart-mode-line)
 
 (require 'init-function)
--- inits/init-smart-mode-line.el
+++ inits/init-smart-mode-line.el
@@ -2,12 +2,8 @@
 (setq sml/theme 'respectful
       sml/no-confirm-load-theme t
       sml/shorten-directory t
-      sml/shorten-modes t)
-
-(setq rm-blacklist '(" SP"
-                     " Helm"
-                     " Undo-Tree"
-                     " WS"))
+      sml/shorten-modes t
+      sml/name-width 20)
 
 (sml/setup)
 
