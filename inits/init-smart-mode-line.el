41a1f5e1723621d64ede83b7d768cc4bf7763e78
Wangzongcan <wang.zongcan@gmail.com>
2015-06-30 10:11:13 +0800

    update

--- inits/init-smart-mode-line.el
+++ inits/init-smart-mode-line.el
@@ -3,7 +3,8 @@
       sml/no-confirm-load-theme t
       sml/shorten-directory t
       sml/shorten-modes t
-      sml/name-width 20)
+      sml/name-width 10
+      sml/mode-width 10)
 
 (sml/setup)
 
