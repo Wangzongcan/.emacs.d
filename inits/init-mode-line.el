23896b7fcde71353e4c929df920b0bc94957f895
Wangzongcan <wang.zongcan@gmail.com>
2015-07-05 00:31:58 +0800

    new mode line

--- inits/init-mode-line.el
+++ inits/init-mode-line.el
@@ -1,33 +1,45 @@
+;; write a function to do the spacing
+(defun simple-mode-line-render (left right)
+  "Return a string of `window-width' length containing LEFT, and RIGHT
+  aligned respectively."
+  (let* ((available-width (- (window-width) (length left) 1)))
+    (format (format " %%s %%%ds" available-width) left right)))
+
 (setq-default mode-line-format
-              (list
-               " - "
+              '((:eval (simple-mode-line-render
+                        ;; left
+                        (format-mode-line
+                         (list
+                          '(:eval (propertize "%b" 'face 'font-lock-keyword-face))
 
-               '(:eval (propertize (format "%s" (window-numbering-get-number-string)) 'face 'font-lock-defaults))
+                          " "
 
-               " - "
+                          '(:eval (propertize "%m" 'face 'font-lock-string-face))
 
-               '(:eval (propertize "%b" 'face 'font-lock-keyword-face))
+                          " "
 
-               " - "
+                          '(:eval
+                            (cond
+                             ((string= evil-state 'visual) "V")
+                             ((string= evil-state 'normal) "N")
+                             ((string= evil-state 'motion) "M")
+                             ((string= evil-state 'insert) "I")
+                             ((string= evil-state 'replace) "R")
+                             ((string= evil-state 'emacs) "E")
+                             ((string= evil-state 'operator) "O")))))
 
-               '(:eval (propertize "%m" 'face 'font-lock-string-face
-                                   'help-echo buffer-file-coding-system))
+                        ;; right
+                        (format-mode-line
+                         (list
+                          '(:eval (propertize (if overwrite-mode "Ovr" "Ins") 'face 'font-lock-constant-face))
 
-               " - "
+                          '(:eval (when (buffer-modified-p)
+                                    (concat ","  (propertize "Mod" 'face 'font-lock-warning-face))))
 
-               '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
-                                   'face 'font-lock-preprocessor-face))
+                          '(:eval (when buffer-read-only
+                                    (concat ","  (propertize "RO" 'face 'font-lock-type-face))))
 
-               '(:eval (when (buffer-modified-p)
-                         (concat ","  (propertize "Mod" 'face 'font-lock-warning-face))))
-
-               '(:eval (when buffer-read-only
-                         (concat ","  (propertize "RO" 'face 'font-lock-type-face))))
-
-               '(:eval (when vc-mode
-                         (concat " -"
-                                 (propertize (format "%s" vc-mode) 'face 'font-lock-string-face))))
-
-               " %-"))
+                          `(vc-mode vc-mode)
+                          ))))))
 
 (provide 'init-mode-line)
