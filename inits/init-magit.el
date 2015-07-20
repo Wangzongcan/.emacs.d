8f1571cbdd8faed8b49fb65a0517db093fc7071c
Wangzongcan <wang.zongcan@gmail.com>
2015-07-20 15:40:18 +0800

    fix

--- inits/init-magit.el
+++ inits/init-magit.el
@@ -1,6 +1,6 @@
 (require 'magit)
 
 (setq magit-status-buffer-switch-function 'switch-to-buffer
-      magit-log-section-arguments '("--graph" "--decorate" "--color"))
+      magit-log-arguments '("--graph" "--decorate" "--color"))
 
 (provide 'init-magit)
