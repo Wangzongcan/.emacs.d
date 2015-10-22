fc14067e53ffa153048c97b24b2aa3eae4dcae19
Wangzongcan <wang.zongcan@gmail.com>
2015-10-22 13:57:03 +0800

    use base16 theme

--- inits/init-theme.el
+++ inits/init-theme.el
@@ -1,7 +1,10 @@
 ;; Theme
 
-(use-package material-theme
-  :init
-  (load-theme 'material t))
+(use-package base16-theme
+ :init
+ (load-theme 'base16-oceanicnext-dark t)
+ (set-face-attribute 'fringe nil
+                     :foreground (face-foreground 'default)
+                     :background (face-background 'default)))
 
 (provide 'init-theme)
