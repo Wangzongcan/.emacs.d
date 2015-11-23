88228c1589a71bef86ef8289eb780e47127491e3
Wangzongcan <wang.zongcan@gmail.com>
2015-11-23 19:37:16 +0800

    缩进为两个空格

--- inits/lang/init-javascript.el
+++ inits/lang/init-javascript.el
@@ -2,6 +2,11 @@
   :ensure t
   :defer t
   :mode (("\\.js$" . js2-mode)
-         ("\\.es6$" . js2-mode)))
+         ("\\.es6$" . js2-mode)
+         ("\\.jsx$" . js2-jsx-mode))
+  :init
+  (setq js-indent-level 2
+        js2-indent-level 2
+        js2-basic-offset 2))
 
 (provide 'init-javascript)
