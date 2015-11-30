c4db28d4fc1c81653f5d0509b2d948afdae04ade
Wangzongcan <wang.zongcan@gmail.com>
2015-11-30 12:40:50 +0800

    修改 emmet-mode 不随 web-mode 启动

--- inits/ext/init-emmet.el
+++ inits/ext/init-emmet.el
@@ -1,8 +1,9 @@
 (use-package emmet-mode
   :ensure t
   :defer t
-  :config
+  :init
   (add-hook 'web-mode-hook 'emmet-mode)
+  :config
   (add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2))))
 
 (provide 'init-emmet)
