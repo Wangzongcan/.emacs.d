958df0477eabefc4c666ec582b272df5788ab9f6
Wangzongcan <wang.zongcan@gmail.com>
2015-12-06 21:26:13 +0800

    启用 ivy

--- inits/ext/init-counsel.el
+++ inits/ext/init-counsel.el
@@ -1,13 +1,12 @@
 (use-package counsel
   :ensure t
-  :bind ("M-x" . counsel-M-x)
-  :config
-  (use-package ivy
-    :init
-    (setq ivy-use-virtual-buffers t)
-    :config
-    (ivy-mode 1)))
+  :bind ("M-x" . counsel-M-x))
 
+(use-package ivy
+  :init
+  (setq ivy-use-virtual-buffers t)
+  :config
+  (ivy-mode 1))
 
 (use-package swiper
   :ensure t
