f8464da08722f10aa0b991ff62b4133a81cdcdb0
Wangzongcan <wang.zongcan@gmail.com>
2015-12-29 15:34:44 +0800

    显示匹配括号

--- inits/ext/init-smartparens.el
+++ inits/ext/init-smartparens.el
@@ -1,7 +1,10 @@
 (use-package smartparens
   :ensure t
+  :diminish smartparens-mode
   :init
   (require 'smartparens-config)
+
+  (show-smartparens-global-mode t)
   (smartparens-global-mode t))
 
 (provide 'init-smartparens)
