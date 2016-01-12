6fddd36ebb504d4b1f0d2043cc09d5b0336da7f3
Wangzongcan <wang.zongcan@gmail.com>
2016-01-12 22:53:23 +0800

    使用 smartparens

--- settings/30-smartparens.el
+++ settings/30-smartparens.el
@@ -4,6 +4,12 @@
   (smartparens-global-mode t)
   (show-smartparens-global-mode t)
   :config
+  (sp-with-modes '(web-mode)
+    (sp-local-pair "<% "  " %>")
+    (sp-local-pair "<%= " " %>")
+    (sp-local-tag "%" "<% "  " %>")
+    (sp-local-tag "=" "<%= " " %>"))
+  
   (add-hook 'prog-mode-hook 'turn-on-smartparens-strict-mode))
 
 (provide '30-smartparens)
--- settings/50-web.el
+++ settings/50-web.el
@@ -3,6 +3,7 @@
   :mode (("\\.html$" . web-mode)
          ("\\.erb$" . web-mode))
   :config
-  (setq web-mode-markup-indent-offset 2))
+  (setq web-mode-markup-indent-offset 2
+        web-mode-enable-auto-pairing nil))
 
 (provide '50-web)
