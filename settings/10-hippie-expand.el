268a5290b89065e22a094a1961d6bcf3b5119b98
Wangzongcan <wang.zongcan@gmail.com>
2016-01-14 16:22:24 +0800

    取消注释

--- settings/10-hippie-expand.el
+++ settings/10-hippie-expand.el
@@ -1,15 +1,15 @@
-;; (use-package hippie-expand
-;;   :bind (("M-/" . hippie-expand))
-;;   :config
-;;   (setq hippie-expand-try-functions-list
-;;         '(try-complete-file-name-partially
-;;           try-complete-file-name
-;;           try-expand-all-abbrevs
-;;           try-expand-list try-expand-line
-;;           try-expand-dabbrev
-;;           try-expand-dabbrev-all-buffers
-;;           try-expand-dabbrev-from-kill
-;;           try-complete-lisp-symbol-partially
-;;           try-complete-lisp-symbol)))
+(use-package hippie-expand
+  :bind (("M-/" . hippie-expand))
+  :config
+  (setq hippie-expand-try-functions-list
+        '(try-complete-file-name-partially
+          try-complete-file-name
+          try-expand-all-abbrevs
+          try-expand-list try-expand-line
+          try-expand-dabbrev
+          try-expand-dabbrev-all-buffers
+          try-expand-dabbrev-from-kill
+          try-complete-lisp-symbol-partially
+          try-complete-lisp-symbol)))
 
 (provide '10-hippie-expand)
