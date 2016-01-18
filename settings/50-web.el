e07e8cefa01a8b8fa8d065c469da1992f832c089
Wangzongcan <wang.zongcan@gmail.com>
2016-01-18 11:14:02 +0800

    style 里 css 的对齐宽度

--- settings/50-web.el
+++ settings/50-web.el
@@ -3,7 +3,9 @@
   :mode (("\\.html$" . web-mode)
          ("\\.erb$" . web-mode))
   :config
-  (setq web-mode-markup-indent-offset 2
+  (setq web-mode-style-padding 2
+        web-mode-css-indent-offset 2
+        web-mode-markup-indent-offset 2
         web-mode-enable-auto-pairing nil))
 
 (provide '50-web)
