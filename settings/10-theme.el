672e5686eeea0dbe1ffc05a9d82eedeef9514090
Wangzongcan <wang.zongcan@gmail.com>
2016-02-18 22:08:57 +0800

    show parens

--- settings/10-theme.el
+++ settings/10-theme.el
@@ -1,12 +1,12 @@
-(use-package monokai-theme
-  :ensure t)
+;; (use-package monokai-theme
+;;   :ensure t)
 
 ;; (use-package spacemacs-theme
 ;;   :ensure t
 ;;   :init
 ;;   (load-theme 'spacemacs-dark t))
 
-;; (use-package dracula-theme
-;;   :ensure t)
+(use-package dracula-theme
+  :ensure t)
 
 (provide '10-theme)
--- settings/10-ui.el
+++ settings/10-ui.el
@@ -19,4 +19,12 @@
 
 (setq linum-format " %4d ")
 
+;; show parent
+(show-paren-mode 1)
+(setq show-paren-delay 0)
+(setq show-paren-style 'expression)
+(set-face-foreground 'show-paren-match-face nil)
+(set-face-background 'show-paren-match-face nil)
+(set-face-underline-p 'show-paren-match-face "white")
+
 (provide '10-ui)
