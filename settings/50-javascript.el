7c3afb333bf767d9ef4e947a0b0d2c876d9eeb6f
Wangzongcan <wang.zongcan@gmail.com>
2016-02-19 22:12:27 +0800

    更新 javascript 的配置

--- settings/50-javascript.el
+++ settings/50-javascript.el
@@ -5,7 +5,8 @@
          ("\\.jsx\\'" . js2-jsx-mode))
   :interpreter (("node" . js2-mode))
   :config
-  (setq js2-indent-level 2
+  (setq js-indent-level 2
+        js2-indent-level 2
         js2-basic-offset 2))
 
 (provide '50-javascript)
