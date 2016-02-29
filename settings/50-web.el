a0535d0b2b1515162d1da0222f4a6c49d2de0b6a
Wangzongcan <wang.zongcan@gmail.com>
2016-02-29 09:47:57 +0800

    update

--- settings/50-web.el
+++ settings/50-web.el
@@ -5,6 +5,8 @@
   :config
   (setq web-mode-style-padding 2
         web-mode-css-indent-offset 2
+        web-mode-attr-indent-offset 2
+        web-mode-code-indent-offset 2
         web-mode-markup-indent-offset 2
         web-mode-enable-auto-pairing t))
 
