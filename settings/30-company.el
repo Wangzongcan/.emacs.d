ca31f112dec52dd01fc7aba7c61fa46fbc72cf51
Wangzongcan <wang.zongcan@gmail.com>
2016-03-03 14:55:14 +0800

    添加 yasnippet 补全

--- settings/30-company.el
+++ settings/30-company.el
@@ -12,6 +12,19 @@
         company-dabbrev-code-everywhere t
         company-dabbrev-code-ignore-case t)
 
+  ;; Add yasnippet support for all company backends
+  ;; https://github.com/syl20bnr/spacemacs/pull/179
+  (defun company-mode/backend-with-yas (backend)
+    (if (and (listp backend) (member 'company-yasnippet backend))
+        backend
+      (append (if (consp backend) backend (list backend))
+              '(:with company-yasnippet))))
+
+  (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
+
+  (bind-keys :map global-map
+             ("C-i" . company-indent-or-complete-common))
+
   (bind-keys :map company-active-map
              ("C-n" . company-select-next)
              ("C-p" . company-select-previous)))
