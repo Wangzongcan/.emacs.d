217000ccada9ec3bc28ad4029bf3fdaffa3772fd
Wangzongcan <wang.zongcan@gmail.com>
2016-04-27 16:54:06 +0800

    删除高度

--- inits/ext/init-spaceline.el
+++ inits/ext/init-spaceline.el
@@ -2,8 +2,8 @@
   :ensure spaceline
   :config
   (spaceline-spacemacs-theme)
-  (setq powerline-height 20
-        powerline-default-separator 'slant
+
+  (setq powerline-default-separator 'slant
         spaceline-highlight-face-func 'spaceline-highlight-face-evil-state))
 
 (provide 'init-spaceline)
