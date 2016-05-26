53b027dbd18be7a00f7ea73cb700f62feb4bc891
Wangzongcan <wang.zongcan@gmail.com>
2016-05-26 23:31:35 +0800

    magit log 参数

--- inits/ext/init-git.el
+++ inits/ext/init-git.el
@@ -1,5 +1,7 @@
 (use-package magit
   :ensure t
-  :defer t)
+  :defer t
+  :config
+  (setq magit-log-arguments '("--graph" "--color" "--decorate" "--show-signature" "-n256" "++order=date")))
 
 (provide 'init-git)
