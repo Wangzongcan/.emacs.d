52c114c95b957c0ac03575ec2cabab45f0b62a00
Wangzongcan <wang.zongcan@gmail.com>
2016-06-11 09:39:23 +0800

    exec-path

--- inits/core/init-path.el
+++ inits/core/init-path.el
@@ -1,5 +1,5 @@
 (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
 
-;; (setq exec-path (append '("/usr/local/bin") exec-path))
+(setq exec-path (append '("/usr/local/bin") exec-path))
 
 (provide 'init-path)
