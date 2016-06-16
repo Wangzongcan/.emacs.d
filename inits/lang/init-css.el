5f5d9fbb2c7b1885c14df55fd5b60c29028b6006
Wangzongcan <wang.zongcan@gmail.com>
2016-06-16 16:51:49 +0800

    css 不开启语法检测

--- inits/lang/init-css.el
+++ inits/lang/init-css.el
@@ -1,4 +1,6 @@
 (use-package css-mode
+  :init
+  (add-hook 'css-mode-hook 'my/css-mode-hook)
   :config
   (setq css-indent-offset 2))
 
@@ -11,5 +13,8 @@
   :mode (("\\.scss$" . scss-mode))
   :config
   (setq scss-compile-at-save nil))
+
+(defun my/css-mode-hook()
+  (flycheck-mode -1))
 
 (provide 'init-css)
