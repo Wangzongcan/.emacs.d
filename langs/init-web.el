b937e7225c48da1107d2c5ae8c08f426cfbc01e0
Wangzongcan <wang.zongcan@gmail.com>
2016-09-19 13:58:25 +0800

    修改 no window 时 web-mode 的配置

--- langs/init-web.el
+++ langs/init-web.el
@@ -1,25 +1,24 @@
+(use-package emmet-mode :ensure t)
+(use-package company-web-html :ensure company-web)
+
 (use-package web-mode
   :ensure t
   :mode (("\\.html\\'" . web-mode)
          ("\\.erb\\'" . web-mode))
   :config
   (setq web-mode-style-padding 2
-        web-mode-css-indent-offset 2
-        web-mode-html-offset 2
         web-mode-script-padding 2
+        web-mode-css-indent-offset 2
         web-mode-attr-indent-offset 2
         web-mode-code-indent-offset 2
-        web-mode-markup-indent-offset 2
-        web-mode-auto-close-style 1
-        web-mode-enable-auto-closing t
-        web-mode-enable-auto-expanding t
-        web-mode-enable-auto-pairing t)
+        web-mode-markup-indent-offset 2)
+
+  (unless window-system
+    (setq web-mode-enable-auto-pairing t
+          web-mode-enable-auto-closing t
+          web-mode-enable-auto-quoting t))
 
   (add-hook 'web-mode-hook 'my/web-mode-hook))
-
-(use-package emmet-mode :ensure t)
-
-(use-package company-web-html :ensure company-web)
 
 (defun my/web-mode-hook ()
   (emmet-mode)
