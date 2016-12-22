7cf5268c04330a3cd8af00cb6efdaa64ed14e423
Wangzongcan <wang.zongcan@gmail.com>
2016-12-22 21:50:46 +0800

    删除空格

--- inits/init-dired.el
+++ inits/init-dired.el
@@ -4,13 +4,13 @@
 
 (defun my/dired-mode-hook ()
   (require 'ls-lisp)
-  
+
   (dired-hide-details-mode t)
-  
+
   (setq dired-dwim-target t
         dired-auto-revert-buffer t
         dired-recursive-copies 'always
-  
+
         ls-lisp-dirs-first t
         ls-lisp-ignore-case t
         ls-lisp-use-insert-directory-program nil))
