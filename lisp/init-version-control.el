ddef4e1d9d8d9b8e0d5630f989efcf737f424175
Wangzongcan <wang.zongcan@gmail.com>
2020-11-19 10:50:26 +0800

    设置 diff-hl fringe 显示

--- lisp/init-version-control.el
+++ lisp/init-version-control.el
@@ -2,11 +2,20 @@
   :bind ("C-x g" . magit-status))
 
 (use-package diff-hl
+  :custom-face
+  (diff-hl-change ((t (:foreground ,(face-background 'highlight) :background nil))))
+  (diff-hl-insert ((t (:background nil))))
+  (diff-hl-delete ((t (:background nil))))
   :hook ((after-init . global-diff-hl-mode)
          (after-init . diff-hl-flydiff-mode))
   :init (setq diff-hl-draw-borders nil)
   :config
   (setq-default fringes-outside-margins t)
+
+  (defun my-diff-hl-fringe-bmp-function (_type _pos)
+    "Fringe bitmap function for use as `diff-hl-fringe-bmp-function'."
+    (define-fringe-bitmap 'my-diff-hl-bmp [224] 1 8 '(center t)))
+  (setq diff-hl-fringe-bmp-function #'my-diff-hl-fringe-bmp-function)
 
   (unless (display-graphic-p)
     (diff-hl-margin-mode 1))
@@ -16,4 +25,4 @@
     (add-hook 'magit-pre-refresh-hook #'diff-hl-magit-pre-refresh)
     (add-hook 'magit-post-refresh-hook #'diff-hl-magit-post-refresh)))
 
-  (provide 'init-version-control)
+(provide 'init-version-control)
