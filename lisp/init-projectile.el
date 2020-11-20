3049b93c65b1d66b438acc82dbe619a6b7ea1901
Wangzongcan <wang.zongcan@gmail.com>
2020-11-20 19:24:30 +0800

    projectile 使用 rg 搜索文件

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -1,7 +1,13 @@
 (use-package projectile
   :bind-keymap ("C-c p" . projectile-command-map)
   :config
-  (setq projectile-completion-system 'ivy))
+  (setq projectile-enable-caching t
+        projectile-completion-system 'ivy
+        projectile-generic-command
+        (let ((rg-cmd ""))
+          (dolist (dir projectile-globally-ignored-directories)
+            (setq rg-cmd (format "%s --glob '!%s'" rg-cmd dir)))
+          (concat "rg -0 --files --color=never --hidden" rg-cmd))))
 
 (use-package counsel-projectile
   :hook (after-init . counsel-projectile-mode))
