7e885a0117c04185107f641737ad0250a3f01943
Wangzongcan <wang.zongcan@gmail.com>
2023-03-08 22:12:29 +0800

    config ruby

--- lisp/init-ruby.el
+++ lisp/init-ruby.el
@@ -4,4 +4,10 @@
   :custom
   (ruby-insert-encoding-magic-comment nil))
 
+(use-package inf-ruby
+  :hook ((ruby-mode . inf-ruby-minor-mode)
+         (compilation-filter . inf-ruby-auto-enter)))
+
+(use-package bundler)
+
 (provide 'init-ruby)
