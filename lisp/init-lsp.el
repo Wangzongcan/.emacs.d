5a42a8c4e1b8d61d00429231136a00f548ef0e6d
Wangzongcan <wang.zongcan@gmail.com>
2024-01-31 14:44:29 +0800

    add lsp-bridge

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -1,6 +1,13 @@
-(use-package lsp-mode
-  :commands (lsp lsp-deferred)
+(use-package lsp-bridge
+  :straight '(lsp-bridge :type git :host github :repo "manateelazycat/lsp-bridge"
+            :files (:defaults "*.el" "*.py" "acm" "core" "langserver" "multiserver" "resources")
+            :build (:not compile))
+  :hook
+  (lsp-bridge-mode . (lambda () (corfu-mode -1)))
   :custom
-  (lsp-headerline-breadcrumb-enable nil))
+  (lsp-bridge-python-command "~/.asdf/shims/python3")
+  :config
+  (setq lsp-bridge-user-langserver-dir (expand-file-name "lsp-bridge/langserver/" no-littering-etc-directory)
+        lsp-bridge-user-multiserver-dir (expand-file-name "lsp-bridge/multiserver/" no-littering-etc-directory)))
 
 (provide 'init-lsp)
