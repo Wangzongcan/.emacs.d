8a5a69a4c846174e9e48ea7cdb95992e4e33cb66
Wangzongcan <wang.zongcan@gmail.com>
2024-08-22 10:15:06 +0800

    支持拼音搜索

--- lisp/init-vertico.el
+++ lisp/init-vertico.el
@@ -2,8 +2,8 @@
 (use-package orderless
   :custom
   (completion-styles '(orderless basic))
-  (completion-category-defaults nil)
-  (completion-category-overrides '((file (styles basic partial-completion)))))
+  (completion-category-overrides '((file (styles basic partial-completion))))
+  (orderless-component-separator #'orderless-escapable-split-on-space))
 
 ;; Vertico
 (use-package vertico
@@ -35,5 +35,14 @@
 ;; Marginalia
 (use-package marginalia
   :hook (after-init . marginalia-mode))
+
+;; Support Pinyin
+(use-package pinyinlib
+  :after orderless
+  :autoload pinyinlib-build-regexp-string
+  :init
+  (defun completion--regex-pinyin (str)
+    (orderless-regexp (pinyinlib-build-regexp-string str)))
+  (add-to-list 'orderless-matching-styles 'completion--regex-pinyin))
 
 (provide 'init-vertico)
